b70c663a8f7bc86ddd5236265f4ad6440de7c6ee
LatchedMicrobe0 <rodrigo.pontes.oliveira.lima@gmail.com>
2022-09-25 10:24:32 -0300

    Testando movimentação via retângulo

--- testingSwapScreens.c
+++ testingSwapScreens.c
@@ -8,7 +8,14 @@ int main(void){
     const int screenWidth = 1600;
     const int screenHeight = 800;
 
-    Vector2 vetorImagem = {630,200};
+    Vector2 vectorImage = {630,200};
+    Rectangle player = {400,150,32,32};
+    Camera2D camera = {0};
+
+    camera.target = (Vector2){player.x + 20.0f, player.y + 20.0f};
+    camera.offset = (Vector2){screenWidth/2.0f,screenHeight/2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
     InitWindow(screenWidth, screenHeight, "HELLCIN");
 
@@ -21,10 +28,29 @@ int main(void){
 
     while(!WindowShouldClose()){
 
+        if(IsKeyDown(KEY_ENTER)){
+                flagInicio = 1;
+        }else if(IsKeyDown(KEY_F11)){
+                ToggleFullscreen();
+        }
+
+        if(IsKeyDown(KEY_RIGHT)){
+            player.x += 2.0;
+        }else if(IsKeyDown(KEY_LEFT)){
+            player.x -= 2.0;
+        }else if(IsKeyDown(KEY_UP)){
+            player.y -= 2.0;
+        }else if(IsKeyDown(KEY_DOWN)){
+            player.y += 2.0;
+        }
+
         if(flagInicio){
 
            BeginDrawing();
-                ClearBackground(RAYWHITE);
+                BeginMode2D(camera);
+                    ClearBackground(RAYWHITE);
+                    DrawRectangleRec(player, RED);
+                EndMode2D();
                 DrawText("Aqui jaz uma história muito nova\nE muito sofrida...", 400, 150, 50, BLACK);
             EndDrawing(); 
 
@@ -33,12 +59,8 @@ int main(void){
                 ClearBackground(RAYWHITE);
 
                 DrawText("HellCIN", 680, 150, 50, BLACK);
-                DrawTextureV(fireTexture, vetorImagem, WHITE);
+                DrawTextureV(fireTexture, vectorImage, WHITE);
                 DrawTextureV(enter_button, (Vector2) {690, 520}, WHITE);
-
-                if(IsKeyDown(KEY_ENTER)){
-                    flagInicio = 1;
-                }
 
             EndDrawing();
 
