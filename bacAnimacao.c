27171ed2b24ee6d4ea84a21deeac1343e33ab1fa
Juliana Silva <julnsilva20@gmail.com>
2022-10-05 09:56:06 -0300

    Adicionando os arquivos de animacoes

--- bacAnimacao.c
+++ bacAnimacao.c
@@ -17,8 +17,9 @@ int main(){
     Texture2D bacAttack = LoadTexture("./Assets/Ball and Chain Bot/attack.png");
     Texture2D bacDeath = LoadTexture("./Assets/Ball and Chain Bot/death.png");
 
+    int bacRun_nSprites = 8;
     int bacRun_Width = bacRun.width;
-    int bacRun_Heigth = bacRun.height/8;
+    int bacRun_Heigth = bacRun.height/bacRun_nSprites;
     
     int bacAttack_Width = bacAttack.width;
     int bacAttack_Heigth = bacAttack.height/8;
@@ -53,62 +54,49 @@ int main(){
     int bacDeath_framespeed = 3;
     int bacDeath_currentframe = 0;
     bool bacDeath_active = false;
-    bool bacDead = false;
-
+    bool IsBacDead = false;
 
     while(!WindowShouldClose()){
 
         bacRun_framecounter++;
-        
         bacDeath_framecounter++;
-
+        bacAttack_framecounter++;
 
         if(IsKeyDown(KEY_RIGHT)){
+
             bacRun_destRect.x +=4;
             
-             if(bacRun_framecounter >= (60/(bacRun_framespeed))){
-                bacRun_framecounter = 0;
-                bacRun_currentframe += 1;
-                if(bacRun_currentframe > 8)  
-                    bacRun_currentframe = 0;
-                bacRun_sourceRect.y = (float)(bacRun_currentframe) * (float)bacRun_Heigth;
+            if(bacRun_sourceRect.width < 0){
+                bacRun_sourceRect.width = -1 * bacRun_sourceRect.width;
+                bacAttack_sourceRect.width = -1 * bacAttack_sourceRect.width;
             }
+
+            bacRun_sourceRect.y = runningAnimations(&bacRun_framecounter, &bacRun_framespeed, &bacRun_currentframe, bacRun_sourceRect.y, bacRun_Heigth, bacRun_nSprites);
+
         }
         if(IsKeyDown(KEY_LEFT)){
+            
             bacRun_destRect.x -=4;
 
-            if(bacRun_framecounter >= (60/(bacRun_framespeed))){
-                bacRun_framecounter = 0;
-                bacRun_currentframe += 1;
-                if(bacRun_currentframe > 8)  
-                    bacRun_currentframe = 0;
-                bacRun_sourceRect.y = (float)(bacRun_currentframe) * (float)bacRun_Heigth;
+            if(bacRun_sourceRect.width > 0){
+                bacRun_sourceRect.width = -1 * bacRun_sourceRect.width;
+                bacAttack_sourceRect.width = -1 * bacAttack_sourceRect.width;
             }
+
+            bacRun_sourceRect.y = runningAnimations(&bacRun_framecounter, &bacRun_framespeed, &bacRun_currentframe, bacRun_sourceRect.y, bacRun_Heigth, bacRun_nSprites);
         }
         if(IsKeyDown(KEY_DOWN)){
             bacRun_destRect.y +=4;
 
-            if(bacRun_framecounter >= (60/(bacRun_framespeed))){
-                bacRun_framecounter = 0;
-                bacRun_currentframe +=1;
-                if(bacRun_currentframe > 8)
-                    bacRun_currentframe = 0;
-                bacRun_sourceRect.y = (float)(bacRun_currentframe) * (float)bacRun_Heigth;
-            }
+            bacRun_sourceRect.y = runningAnimations(&bacRun_framecounter, &bacRun_framespeed, &bacRun_currentframe, bacRun_sourceRect.y, bacRun_Heigth, bacRun_nSprites);
         }
         if(IsKeyDown(KEY_UP)){
             bacRun_destRect.y -=4;
 
-            if(bacRun_framecounter >= (60/(bacRun_framespeed))){
-                bacRun_framecounter = 0;
-                bacRun_currentframe +=1;
-                if(bacRun_currentframe > 8)
-                    bacRun_currentframe = 0;
-                bacRun_sourceRect.y = (float)(bacRun_currentframe) * (float)bacRun_Heigth;
-            }
+            bacRun_sourceRect.y = runningAnimations(&bacRun_framecounter, &bacRun_framespeed, &bacRun_currentframe, bacRun_sourceRect.y, bacRun_Heigth, bacRun_nSprites);
         }
         
-        bacAttack_framecounter++;
+        
         
         if(IsKeyPressed(KEY_B) && !bacAttack_active)
            bacAttack_active = true;
@@ -137,10 +125,11 @@ int main(){
                     }
                     
                     bacAttack_framecounter = 0;
-                }    
+                }  
+               bacAttack_sourceRect.y = (float)(bacAttack_currentframe) * (float)(bacAttack_Heigth);    
             } 
                 
-        }
+        }        
         
         if(bacDeath_active){
             
@@ -157,20 +146,22 @@ int main(){
                         
                         bacDeath_currentframe = 4;
                         bacDeath_active = false;
-                        bacDead = true;
+                        IsBacDead = true;
                     
                     }
                     
                     bacDeath_framecounter = 0;
                 }    
             }            
-        }
-        
-        bacAttack_sourceRect.y = (float)(bacAttack_currentframe) * (float)(bacAttack_Heigth);  
+        } 
 
-        bacDeath_sourceRect.y = (float)(bacDeath_currentframe) * (float)(bacDeath_Heigth);        
+        bacDeath_sourceRect.y = (float)(bacDeath_currentframe) * (float)(bacDeath_Heigth);   
+
+        mapBorders(&bacRun_destRect.x,&bacRun_destRect.y,bacRun_destRect.width,bacRun_destRect.height, 1600,800);  
+        mapBorders(&bacAttack_destRect.x,&bacAttack_destRect.y,bacAttack_destRect.width,bacAttack_destRect.height, 1600,800);  
+        mapBorders(&bacDeath_destRect.x,&bacDeath_destRect.y,bacDeath_destRect.width,bacDeath_destRect.height, 1600,800);   
                 
-        if(bacAttack_active && !bacDead){
+        if(bacAttack_active && !IsBacDead){
             BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawTexturePro(bacAttack, bacAttack_sourceRect, bacAttack_destRect, origin, rotation, RAYWHITE);
@@ -182,13 +173,13 @@ int main(){
             DrawTexturePro(bacDeath, bacDeath_sourceRect, bacDeath_destRect, origin, rotation, RAYWHITE);            
             EndDrawing();
         
-        }else if(!bacDead){      
+        }else if(!IsBacDead){      
             BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawTexturePro(bacRun, bacRun_sourceRect, bacRun_destRect, origin, rotation, RAYWHITE);            
             EndDrawing();   
             
-        }else if(bacDead){
+        }else if(IsBacDead){
             BeginDrawing();            
             ClearBackground(RAYWHITE);            
             DrawTexturePro(bacDeath, bacDeath_sourceRect, bacDeath_destRect, origin, rotation, RAYWHITE);            
--- bacRun.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "raylib.h"
-#include "animations.h"
-#include "collision.h"
-
-int main(void){
-    
-}
